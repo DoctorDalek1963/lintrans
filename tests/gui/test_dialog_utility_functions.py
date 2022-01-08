2da6a4daa88e50f4e80ebd9a0d690f3fabe96623
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-01-08 13:04:44 +0000

    Add tests for is_valid_float() utility function

---
+++ tests/gui/test_dialog_utility_functions.py
@@ -0,0 +1,19 @@
+"""Test the utility functions for GUI dialog boxes."""
+
+import pytest
+from lintrans.gui.dialogs.define_new_matrix import is_valid_float
+
+valid_floats: list[str] = [
+    '0', '1', '3', '-2', '123', '-208', '1.2', '-3.5', '4.252634', '-42362.352325'
+]
+
+invalid_floats: list[str] = [
+    '', 'pi', 'e', '1.2.3', '1,2', '-', '.', 'None', 'no', 'yes', 'float'
+]
+
+
+@pytest.mark.parametrize('inputs,output', [(valid_floats, True), (invalid_floats, False)])
+def test_is_valid_float(inputs: list[str], output: bool) -> None:
+    """Test the is_valid_float() function."""
+    for inp in inputs:
+        assert is_valid_float(inp) == output
