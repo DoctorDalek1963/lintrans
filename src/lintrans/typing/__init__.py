ef4eb80205a737a5208d357d3079aa861aba2a20
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-02-04 17:58:44 +0000

    Clean up typing/__init__.py

--- src/lintrans/typing/__init__.py
+++ src/lintrans/typing/__init__.py
@@ -7,18 +7,20 @@ from typing import Any, TypeGuard
 from numpy import ndarray
 from nptyping import NDArray, Float
 
-__all__ = ['MatrixType', 'is_matrix_type', 'MatrixParseList']
+__all__ = ['is_matrix_type', 'MatrixType', 'MatrixParseList']
 
-#: The type that represents a 2x2 matrix as a NumPy array
 MatrixType = NDArray[(2, 2), Float]
+"""The type that represents a 2x2 matrix as a NumPy array."""
 
-#: The type of the list that :func:`lintrans.matrices.parse.parse_matrix_expression` returns
 MatrixParseList = list[list[tuple[str, str, str]]]
+"""The type of the list that :func:`lintrans.matrices.parse.parse_matrix_expression` returns."""
 
 
 def is_matrix_type(matrix: Any) -> TypeGuard[NDArray[(2, 2), Float]]:
     """Check if the given value is a valid matrix type.
 
-    .. note:: This function is a TypeGuard, meaning if it returns True, then the passed value must be a MatrixType.
+    .. note::
+       This function is a TypeGuard, meaning if it returns True, then the
+       passed value must be a :attr:`lintrans.typing.MatrixType`.
     """
     return isinstance(matrix, ndarray) and matrix.shape == (2, 2)
