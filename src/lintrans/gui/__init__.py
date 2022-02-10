be22c4aec5e3f5dbd0f1ec2c5200630d599f86e2
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-02-10 09:11:18 +0000

    Update __init__.py files

--- src/lintrans/gui/__init__.py
+++ src/lintrans/gui/__init__.py
@@ -1,6 +1,6 @@
 """This package supplies the main GUI and associated dialogs for visualization."""
 
-from . import dialogs, plots, settings
+from . import dialogs, plots, settings, validate
 from .main_window import main
 
-__all__ = ['dialogs', 'main', 'plots', 'settings']
+__all__ = ['dialogs', 'main', 'plots', 'settings', 'validate']
--- src/lintrans/gui/dialogs/__init__.py
+++ src/lintrans/gui/dialogs/__init__.py
@@ -1,6 +1,6 @@
 """This package provides separate dialogs for the main GUI.
 
-These dialogs are mainly for defining new matrices in different ways.
+These dialogs are for defining new matrices in different ways and editing settings.
 """
 
 from .define_new_matrix import DefineAsAnExpressionDialog, DefineDialog, DefineNumericallyDialog, DefineVisuallyDialog
