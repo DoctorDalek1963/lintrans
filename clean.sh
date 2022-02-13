d8b19dbd2c321fd3a07e5d3be10c3f86e1456efc
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-02-13 16:59:14 +0000

    Create initial write-up structure

---
+++ clean.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+rm *.log 2> /dev/null
+rm *.aux 2> /dev/null
+rm *.dvi 2> /dev/null
+rm *.lof 2> /dev/null
+rm *.lot 2> /dev/null
+rm *.bit 2> /dev/null
+rm *.idx 2> /dev/null
+rm *.glo 2> /dev/null
+rm *.bbl 2> /dev/null
+rm *.bcf 2> /dev/null
+rm *.ilg 2> /dev/null
+rm *.toc 2> /dev/null
+rm *.ind 2> /dev/null
+rm *.out 2> /dev/null
+rm *.blg 2> /dev/null
+rm *.fdb_latexmk 2> /dev/null
+rm *.fls 2> /dev/null
+rm *.run.xml 2> /dev/null
+rm *.synctex.gz 2> /dev/null
