5f4fa3176158dc5fbe47deb0aac5c5d2e121e0b6
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-02-15 13:20:24 +0000

    Fix workflows.bash

--- workflows.bash
+++ workflows.bash
@@ -2,7 +2,7 @@
 
 IFS=$'\n'
 
-for command in $(cat .github/workflows/*.yml | grep -Po '(?<=run: )[^|]+'); do
+for command in $(cat .github/workflows/*.yaml | grep -Po '(?<=run: )[^|]+'); do
 	echo -e "\e[1;32mRUNNING:\e[0m '$command'"
 	eval "$command" || echo -e "\e[1;31mFAILED:\e[0m '$command'"
 	echo
