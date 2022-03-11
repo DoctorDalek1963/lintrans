dca45c5a9b7b58f31b1a2b5ef635f7fe77e86f51
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-11 17:06:13 +0000

    Add automated release publishing

--- workflows.bash
+++ workflows.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-IFS=$'\n'
+# Don't run compile-for-release.yaml locally, because that won't work properly
+for file in .github/workflows/compile-docs.yaml .github/workflows/docstrings.yaml .github/workflows/linting.yaml .github/workflows/tests.yaml .github/workflows/type_checks.yaml; do
+	IFS=$'\n'
 
-for command in $(cat .github/workflows/*.yaml | grep -Po '(?<=run: )[^|]+'); do
-	echo -e "\e[1;32mRUNNING:\e[0m '$command'"
-	eval "$command" || echo -e "\e[1;31mFAILED:\e[0m '$command'"
-	echo
-	echo
-	echo
+	for command in $(cat $file | grep -Po '(?<=run: )[^|]+'); do
+		echo -e "\e[1;32mRUNNING:\e[0m '$command'"
+		eval "$command" || echo -e "\e[1;31mFAILED:\e[0m '$command'"
+		echo
+		echo
+		echo
+	done
+
+	unset IFS
 done
-
-unset IFS
