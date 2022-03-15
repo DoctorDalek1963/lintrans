16273047c18dadb31c38324c93143da4c052063b
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-15 14:26:28 +0000

    Fix clean.sh shebang

--- clean.sh
+++ clean.sh
@@ -1,10 +1,10 @@
+#!/usr/bin/env sh
+
 # lintrans - The linear transformation visualizer
 # Copyright (C) 2021-2022 D. Dyson (DoctorDalek1963)
 
 # This program is licensed under GNU GPLv3, available here:
 # <https://www.gnu.org/licenses/gpl-3.0.html>
-
-#!/usr/bin/env sh
 
 rm *.log 2> /dev/null
 rm *.aux 2> /dev/null
