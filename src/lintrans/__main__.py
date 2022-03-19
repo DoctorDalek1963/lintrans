ffc603f1bf049811cb927f879ce7989456f6a537
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-19 23:05:37 +0000

    Support --help and --version flags in __main__

--- src/lintrans/__main__.py
+++ src/lintrans/__main__.py
@@ -6,14 +6,50 @@
 # This program is licensed under GNU GPLv3, available here:
 # <https://www.gnu.org/licenses/gpl-3.0.html>
 
-"""This module very simply runs the app by calling :func:`lintrans.gui.main_window.main`.
-
-This allows the user to run the app like ``python -m lintrans`` from the command line.
-"""
+"""This module provides a :func:`main` function to interpret command line arguments and run the program."""
 
 import sys
+from textwrap import dedent
 
+from lintrans import __version__
 from lintrans.gui import main_window
 
+
+def main(prog_name: str, args: list[str]) -> None:
+    """Interpret program-specific command line arguments and run the main window in most cases.
+
+    If the user supplies --help or --version, then we simply respond to that and then return.
+    If they don't supply either of these, then we run :func:`lintrans.gui.main_window.main`.
+
+    ``prog_name`` is ``sys.argv[0]`` when this script is run with ``python -m lintrans``.
+
+    :param str prog_name: The name of the program
+    :param list[str] args: The other arguments to the program
+    """
+    if '-h' in args or '--help' in args:
+        print(dedent(f'''
+        Usage: {prog_name} [option]
+
+        Options:
+            -h, --help       Display this help text and exit
+            -V, --version    Display the version information and exit
+
+        Any other options will get passed to the QApplication constructor.
+        If you don't know what that means, then don't provide any arguments and just the run the program.'''[1:]))
+
+    elif '-V' in args or '--version' in args:
+        print(dedent(f'''
+        lintrans (version {__version__})
+        The linear transformation visualizer
+
+        Copyright (C) 2021-2022 D. Dyson (DoctorDalek1963)
+
+        This program is licensed under GNU GPLv3, available here:
+        <https://www.gnu.org/licenses/gpl-3.0.html>'''[1:]))
+
+    else:
+        main_window.main(args)
+
+
 if __name__ == '__main__':
-    main_window.main(sys.argv)
+    main(sys.argv[0], sys.argv[1:])
