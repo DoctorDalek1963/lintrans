cdf2d5eeaf615bca76ceb72bf07e02c635dc7731
DoctorDalek1963 <dyson.dyson@icloud.com>
2022-08-20 21:09:45 +0100

    Move single test from test_misc.py

--- tests/backend/matrices/matrix_wrapper/test_setting_and_getting.py
+++ tests/backend/matrices/matrix_wrapper/test_setting_and_getting.py
@@ -223,3 +223,30 @@ def test_set_type_error(new_wrapper: MatrixWrapper) -> None:
     for value in invalid_values:
         with pytest.raises(TypeError):
             new_wrapper['M'] = value
+
+
+def test_get_expression(test_wrapper: MatrixWrapper) -> None:
+    """Test the get_expression method of the MatrixWrapper class."""
+    test_wrapper['N'] = 'A^2'
+    test_wrapper['O'] = '4B'
+    test_wrapper['P'] = 'A+C'
+
+    test_wrapper['Q'] = 'N^-1'
+    test_wrapper['R'] = 'P-4O'
+    test_wrapper['S'] = 'NOP'
+
+    assert test_wrapper.get_expression('A') is None
+    assert test_wrapper.get_expression('B') is None
+    assert test_wrapper.get_expression('C') is None
+    assert test_wrapper.get_expression('D') is None
+    assert test_wrapper.get_expression('E') is None
+    assert test_wrapper.get_expression('F') is None
+    assert test_wrapper.get_expression('G') is None
+
+    assert test_wrapper.get_expression('N') == 'A^2'
+    assert test_wrapper.get_expression('O') == '4B'
+    assert test_wrapper.get_expression('P') == 'A+C'
+
+    assert test_wrapper.get_expression('Q') == 'N^-1'
+    assert test_wrapper.get_expression('R') == 'P-4O'
+    assert test_wrapper.get_expression('S') == 'NOP'
