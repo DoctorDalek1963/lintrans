d4f478321590f9f1c23f7046ce3d7963d131c04c
DoctorDalek1963 <dyson.dyson@icloud.com>
2022-09-08 10:26:41 +0100

    Create simple installer for Linux platforms

---
+++ installers/install-lintrans-Linux.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+create_desktop_file() {
+	rm -f doctordalek1963-lintrans.desktop
+	touch doctordalek1963-lintrans.desktop
+	echo "[Desktop Entry]"                          >> doctordalek1963-lintrans.desktop
+	echo "Type=Application"                         >> doctordalek1963-lintrans.desktop
+	echo "Version=1.5"                              >> doctordalek1963-lintrans.desktop
+	echo "Name=lintrans"                            >> doctordalek1963-lintrans.desktop
+	echo "Comment=Linear transformation visualizer" >> doctordalek1963-lintrans.desktop
+	echo "#Icon=TODO"                               >> doctordalek1963-lintrans.desktop
+	echo "Exec='$1' %f"                             >> doctordalek1963-lintrans.desktop
+	echo "Terminal=false"                           >> doctordalek1963-lintrans.desktop
+	echo "Categories=Education"                     >> doctordalek1963-lintrans.desktop
+}
+
+echo "Welcome to the lintrans installer!"
+
+latest_version="$(curl -sL https://github.com/DoctorDalek1963/lintrans/releases/latest | \grep -Po '(?<=/DoctorDalek1963/lintrans/releases/download/v)\d+\.\d+\.\d+(?=/lintrans-Linux)')"
+binary_url="https://github.com/DoctorDalek1963/lintrans/releases/download/v${latest_version}/lintrans-Linux-${latest_version}"
+
+echo "The latest release is lintrans v${latest_version}\n"
+
+prefix="$HOME/.local/bin"
+read -p "Please enter an installation prefix (leave empty for ${prefix}): " prefix_input
+
+if [ -n "$prefix_input" ]; then
+	prefix="$prefix_input"
+fi
+
+filename="${prefix}/lintrans"
+
+if [ -x "$filename" ]; then
+	current_version="$($filename --version | \grep -Po '(?<=lintrans \(version )\d+\.\d+\.\d+(?=\))')"
+
+	if [ "$current_version" = "$latest_version" ]; then
+		echo "\nYou've got the latest version of lintrans installed there already!"
+		exit 0
+	fi
+fi
+
+echo "\nNow downloading the lintrans binary..."
+wget "$binary_url" -O lintrans-binary -q --show-progress
+
+mv lintrans-binary "$filename"
+chmod +x "$filename"
+
+echo "\nNow installing the XDG .desktop file..."
+create_desktop_file "$filename"
+xdg-desktop-menu install --mode user doctordalek1963-lintrans.desktop
+rm -f doctordalek1963-lintrans.desktop
+
+echo "\nThanks for installing lintrans!"
